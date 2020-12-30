56e8c29fb01a2673ebb7d4a77d87e69c06d45e4c
Menoita99 <rui.menoita@gmail.com>
2020-12-30 16:50:02 +0000

    update

--- AI_SnakeGame/src/main/java/com/nn/train/GeneticEngine.java
+++ AI_SnakeGame/src/main/java/com/nn/train/GeneticEngine.java
@@ -1,58 +1,29 @@
 package com.nn.train;
 
-import java.io.File;
-import java.io.IOException;
-
-import com.neural.NeuralNetwork;
-
-import io.jenetics.DoubleGene;
-import io.jenetics.Mutator;
-import io.jenetics.Optimize;
-import io.jenetics.RouletteWheelSelector;
-import io.jenetics.SinglePointCrossover;
-import io.jenetics.TournamentSelector;
-import io.jenetics.engine.Engine;
-import io.jenetics.engine.EvolutionResult;
-import io.jenetics.engine.EvolutionStatistics;
+import com.gui.Gui;
 
 public class GeneticEngine {
 
-	private Engine<DoubleGene, Double> engine;
-
-
-	public void setUpEngine() {
-//		engine = Engine.builder(new GeneticTraining())
-//				.optimize(Optimize.MAXIMUM)
-//				.populationSize(50)
-//				.offspringFraction(0.6)
-//				.survivorsSelector (new TournamentSelector <>(5)) 
-//				.offspringSelector (new RouletteWheelSelector <>() ) 
-//				.alterers(new Mutator<>(0.02), new SinglePointCrossover<>(0.8))
-//				.build();
-		
-	}
-
-
-	public EvolutionResult<DoubleGene, Double> evaluate(EvolutionStatistics<Double,?> statistics) {
-		EvolutionResult<DoubleGene, Double> result = engine.stream()
-				.limit(100)
-				.peek(er -> System.out.println("Fitness: "+er.bestPhenotype().fitness()))
-				.peek(statistics)
-				.collect(EvolutionResult.toBestEvolutionResult());
-		System.out.println(statistics);
-		return result;
-	}
 	
 
 	public static void main(String[] args) {
-//		EvolutionStatistics<Double,?> statistics = EvolutionStatistics.ofNumber();
-		GeneticEngine gan = new GeneticEngine();
-		gan.setUpEngine();
-		File f = new File("C:\\Users\\car31\\OneDrive\\Desktop\\NeuralNetwork.nn");
-		try {
-			f.createNewFile();
-		} catch (IOException e) {
-			e.printStackTrace();
+		Population pop = new Population(2000);
+		int gens = 100;
+		int i = 0;
+		int bestScore = 0;
+		while ( i < gens) {
+			if(pop.done()) {
+				bestScore = pop.getBestSnake().getScore();
+				pop.calculateFitness();
+				pop.naturalSelection();
+				System.out.println("Gen : "+i+" Score: "+bestScore);
+			} else {
+				pop.update();
+			}
 		}
+		
+		Gui.launch(args);
+//		Gui.getInstance().
 	}
+
 }
---
+++ AI_SnakeGame/src/main/java/com/nn/train/Population.java
@@ -0,0 +1,29 @@
+package com.nn.train;
+
+public class Population {
+
+	public Population(int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public boolean done() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void calculateFitness() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void naturalSelection() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
