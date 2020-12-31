3ae323c44c3d6fe74c8968f0aa327f56a7c01031
Menoita99 <rui.menoita@gmail.com>
2020-12-31 01:21:13 +0000

    update

--- AI_SnakeGame/src/main/java/com/nn/train/GeneticEngine.java
+++ AI_SnakeGame/src/main/java/com/nn/train/GeneticEngine.java
@@ -1,16 +1,19 @@
 package com.nn.train;
 
+
 import com.gui.Gui;
 import com.gui.Launch;
+import com.logic.Snake;
+
 
 public class GeneticEngine {
 
-	
+
 
 	public static void main(String[] args) throws InterruptedException {
 		System.out.println("comecei");
-		Population pop = new Population(500);
-		int gens = 200;
+		Population pop = new Population(1000);
+		int gens = 400;
 		int i = 0;
 		long start = System.currentTimeMillis();
 		while (i < gens) {
@@ -27,7 +30,15 @@ public class GeneticEngine {
 				pop.update();
 			}
 		}
-		
+		System.out.println("TEST BEST");
+		for (int j = 0; j < 3; j++) {
+			Snake s = new Snake(pop.getBestSnake().getBrain());
+			while(!s.isDead()) {
+				s.look();
+				s.thinkAndMove();
+			}
+			System.out.println("Test "+j+" score "+s.getScore()+" fitness "+s.calculateFitness());
+		}
 		new Thread(() -> Launch.main(args)).start();
 		Thread.sleep(1000);
 		Gui.getINSTANCE().playWithNeuralNetwork(pop.getBestSnake().getBrain());
